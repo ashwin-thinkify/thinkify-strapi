4ed9dcfb94f9e8c9eaa7f254ab07e1ebc95b00b8
ashwin-thinkify <ak@thinkify.io>
2021-12-17 11:47:54 +0530

    Updated the content to empty

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '76df7f4aba5dfcf1204be7e625b0cbda'),
-    },
-  },
-});
--- extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '44a3b59f-29d4-4359-86a7-c00fe37226f1'
-};
